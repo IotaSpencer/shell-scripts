210ec8030cda6c71dbb04a566576ba04146c01a2
Ken Spencer <iota@electrocode.net>
2016-01-26 12:04:30 -0500

    So much change o_o

--- inspigen/generator.rb
+++ inspigen/generator.rb
@@ -6,7 +6,133 @@ class Gen
   @@conf = "Inspircd"
   def self.conf
     STDERR.puts "You have selected the #{@@conf.downcase}.conf generator"
-    gen_conf
+    a = HighLine.new($stdin, $stderr)
+    # Conf generation
+    # <server>
+    a.say "Alright, first off"
+    a.say \
+      "1. Lets get the irc.server.name
+      2. Your server info/server description/sdesc
+      3. Your numeric. (If ElectroCode, ask Iota, otherwise talk to your network's routing team or whoever manages linking)"
+    a.say "This will be your <server> block"
+    me_name = a.ask "Server Name? "
+    me_info = a.ask "Server Description? "
+    me_id = a.ask("SID? ") do |q|
+      q.validate = /\d[0-9A-Z][0-9A-Z]/
+    end
+    me_net = a.ask "What Network Name? "
+    # <admin>
+    a.say "Your <admin> lines.."
+    admin_name = a.ask "Admin Real Name? "
+    admin_nick = a.ask "Admin Nick? "
+    admin_email = a.ask("Admin Email? ") do |q|
+      q.validate = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+    end
+    # <connect> in Inspircd doesn't configure servers
+    # so this is all client related options
+    a.say "Alright, the next bit will get your settings for your <connect> block"
+    a.say "First thing, since conf generation usually means you're starting out, we're going to default to allow."
+    a.say "You can generate more <connect> blocks using the the '-t connect' options when starting the script."
+    a.say "We also default to allow=\"*\""
+
+    a.say \
+      "Ping Frequency: The number of seconds between pings from the server (90-240 recommended)
+      This is the frequency at which the server pings you.
+      You want to set this low enough that you have accurate/real connections,
+      but also high enough for some laggy or 'stupid' clients"
+    connect_pingfreq = a.ask("? ", Integer) do |q|
+      q.default = 240
+      q.in = 0..300
+      q.answer_or_default
+    end
+    a.say \
+      "TimeOut: This is the amount of seconds the server will wait before disconnecting a
+      user when doing registration (the auth aka. /nick /user, /pass)"
+    connect_timeout = a.ask("? ", Integer) do |q|
+      q.default = 240
+      q.in = 0..300
+      q.answer_or_default
+    end
+
+    a.say "Max Local Clients: Specifies the maximum amount of clients <%= color(\"per IP\", :red, :bold) %> on this server
+    Note: 3-5 recommended "
+    connect_localmax = a.ask("? ", Integer) do |q|
+      q.default = 5
+      q.in = 1..1000
+      q.answer_or_default
+    end
+
+    a.say "Max Global Clients: Specifies the maximum amount of clients <%= color(\"per IP\", :red, :bold) %> network-wide
+    Note: 3-5 recommended "
+    connect_globalmax = a.ask("? ", Integer) do |q|
+      q.default = 5
+      q.in = 1..1000
+      q.answer_or_default
+    end
+    a.say \
+      "Max Channels: Specifies the maximum number of channels users in this block can join.
+      This overrides every other maxchans setting.
+      Note: The validation range goes to 1000, if you want to increase it, enter a random
+      number or accept the default. "
+    connect_maxchans = a.ask("? ", Integer) do |q|
+      q.default = 100
+      q.in = 20..1000
+      q.answer_or_default
+    end
+    a.say "Send Quota(SendQ): Amount of data that can be in the send queue
+    Note: ~100000+ recommended "
+    connect_sendq = a.ask("? ", Integer) do |q|
+      q.default = 131074
+      q.in = 0..1000000
+      q.answer_or_default
+    end
+    a.say "Receive Quota(RecvQ): Amount of data that can be in the receive queue
+    Note: 3000-8000 recommended "
+    connect_recvq = a.ask("? ", Integer) do |q|
+      q.default = 4096
+      q.in = 3000..10000
+      q.answer_or_default
+    end
+    a.say "<%= color(\"-----------------------------------\", :red, :bold) %>"
+
+    # print_conf
+
+    puts \
+"/*
+# - Regular comment
+// - Another comment
+/*text*/
+# and /* */ can be inline comments
+and as you can see /**/ can also be multiline comments
+*/
+"
+#   me {}
+    puts \
+"<server
+  name=\"#{me_name}\"
+  description=\"#{me_info}\"
+  id=\"#{me_id}\"
+  network=\"#{me_net}\">"
+#   <admin>
+    puts \
+"<admin
+  name=\"#{admin_name}\"
+  nick=\"#{admin_nick}\"
+  email=\"#{admin_email}\">"
+#   <connect>
+    puts \
+"<connect
+  allow=\"*\"
+  maxchans=\"#{connect_maxchans}\"
+  timeout=\"#{connect_timeout}\"
+  pingfreq=\"#{connect_pingfreq}\"
+  sendq=\"#{connect_sendq}\"
+  recvq=\"#{connect_recvq}\"
+  localmax=\"#{connect_localmax}\"
+  globalmax=\"#{connect_globalmax}\"
+  useident=\"no\"
+  limit=\"5000\"
+  modes=\"#{connect_umodes}\">"
   end
   def self.opers
     STDERR.puts "You have selected the #{@@conf} #{__method__.to_s}.conf generator"
@@ -21,56 +147,13 @@ class Gen
     STDERR.puts "You have selected the #{@@conf} #{__method__.to_s}.conf generator"
   end
 end
-def gen_conf
-# Conf generation
-    # <server >
-    print("Alright, first off", file=sys.stderr)
-    print("1. Lets get the irc.server.name", file=sys.stderr)
-    print("2. Your server info/server description/sdesc", file=sys.stderr)
-    print("3. Your numeric. (If ElectroCode, ask Iota, otherwise talk to your network's routing team or whoever manages linking)", file=sys.stderr)
-    print("This is your me {} block", file=sys.stderr)
-    me_name = input()
-    me_info = input()
-    me_numeric = input()
-    checkInt(me_numeric)
-    # Admin {}
-    print("Your admin {} lines.. ", file=sys.stderr)
-    while True:
-        line = input()
-        if not line:
-            break
-        admin_block.append(line)
+
+=begin
     #Class {}
-    print("Alright, the next bit will get your settings for your 'clients' class", file=sys.stderr)
-    print("Ping Frequency: The number of seconds between pings from the server (90-180 recommended) -- This is your PING/PONG from the server", file=sys.stderr)
-    client_pingfreq = input()
-    checkInt(client_pingfreq)
-    print("Max Clients: Specifies the maximum amount of clients in this class (500 recommended)", file=sys.stderr)
-    print(colored("Note", "red", attrs=["bold"]) + ": An example being this, if you maxclients is only 10, then you can only have 10 people on the server " + colored("TOTAL", "red", attrs=["bold"]) + "!", file=sys.stderr)
-    client_max = input()
-    checkInt(client_max)
-    print("Send Quota(SendQ): Amount of data that can be in the send queue (100000 recommended)", file=sys.stderr)
-    client_sendq = input()
-    checkInt(client_sendq)
-    print("Receive Quota(RecvQ): Amount of data that can be in the receive queue (3000-8000 recommended)", file=sys.stderr)
-    client_recvq = input()
-    checkInt(client_recvq)
-    print(colored("-----------------------------------", "red", attrs=["bold"]), file=sys.stderr)
-    print("This is your 'server(s)' class", file=sys.stderr)
-    print("Ping Frequency: The number of seconds between the server pinging its links (90-180 recommended)", file=sys.stderr)
-    server_pingfreq = input()
-    checkInt(server_pingfreq)
-    print("Connect Frequency: The number of seconds between each connection attempt (100 is recommended", file=sys.stderr)
-    server_connfreq = input()
-    checkInt(server_connfreq)
-    print("Max Clients: For servers this means how many servers can be linked to this one server. (May conflict with leafdepth if set <5.. Recommended is 10)", file=sys.stderr)
-    server_max = input()
-    checkInt(server_max)
-    print("Send Quota(SendQ): Amount of data that can be in the send queue (1000000 recommended)", file=sys.stderr)
-    server_sendq = input()
+
     checkInt(server_sendq)
-    print(colored("---------------------------------------------------------", "red", attrs=["bold"]), file=sys.stderr)
-    print("Printing/Writing...", file=sys.stderr)
+    print(colored("---------------------------------------------------------", "red", attrs=["bold"]))
+    print("Printing/Writing...")
     # Conf printing
     ##Default text
     print("""
@@ -118,12 +201,12 @@ class %s {"""% classes)
 };
 """% (client_pingfreq, client_max, client_sendq, client_recvq))
 def allow():
-    print("""The next few questions will ask you for directives for an allow block""", file=sys.stderr)
-    print("""Alright, what user@ip will be allowed? Use '.' as an entry for this to enter *@*""", file=sys.stderr)
+    print("""The next few questions will ask you for directives for an allow block""")
+    print("""Alright, what user@ip will be allowed? Use '.' as an entry for this to enter *@*""")
     allow_ip = input()
-    print("""What user@host will be allowed? Use '.' to enter *@*""", file=sys.stderr)
+    print("""What user@host will be allowed? Use '.' to enter *@*""")
     allow_host = input()
-    print("""What connection class does this allow {} belong in""", file=sys.stderr)
+    print("""What connection class does this allow {} belong in""")
     allow_class = input()
     print(
 """
@@ -134,7 +217,7 @@ If you want to use a different encryption mechanism then enter in this fashion
 If you don't want a password, enter "." for both entries, if you don't want encryption on your password, enter "-"(minus/hyphen/em dash) for your method
     PASSWORD
     -
-"""% (colored("password", "red", attrs=["bold"]), colored("method", "red", attrs=["bold"])), file=sys.stderr)
+"""% (colored("password", "red", attrs=["bold"]), colored("method", "red", attrs=["bold"])))
     allow_passwd = input()
     allow_passwd_method = input()
     print(
@@ -143,7 +226,7 @@ How many clients can connect via this block?
 If you chose '.'/*@* as host and ip then make sure to set this pretty high
 Otherwise 5 is a good default, as that is the default connection limit for clones
 If you're making Session Limit Increases through this then set this to as high as you need it.
-""", file=sys.stderr)
+""")
     allow_max = input()
     checkInt(allow_max)
     print(
@@ -154,11 +237,11 @@ If you allow znc users on IPV6 then you should set this to 128
 Otherwise, due to current address allocation policies to residents, 64 is the
 recommended clone mask.
 Enter "." to leave this directive out of the block.
-""", file=sys.stderr)
+""")
     allow_clone_mask = input()
 # Allow printing
-    print("Printing/Redirecting to file", file=sys.stderr)
-    print(colored("""-------------------------------------------------------""", "red", attrs=["bold"]), file=sys.stderr)
+    print("Printing/Redirecting to file")
+    print(colored("""-------------------------------------------------------""", "red", attrs=["bold"]))
     print("""allow {""")
     if allow_ip:
         if allow_ip == ".":
@@ -194,14 +277,14 @@ Enter "." to leave this directive out of the block.
 def oper():
     print(
 """Here's the oper block generator
-""", file=sys.stderr)
+""")
     print(
-colored("""---------------------------------""", "red", attrs=["bold"]), file=sys.stderr)
+colored("""---------------------------------""", "red", attrs=["bold"]))
     print(
 """
 Name of the oper.. 'bob' 'Iota' as an example.
 Case does count, so 'iota' 'IOTA' 'Iota' 'IoTa' are all different
-""", file=sys.stderr)
+""")
     oper_name = input()
     print(
 """
@@ -212,7 +295,7 @@ user2@host2
 user3@host3
 ...
 To end, enter a blank newline.
-""", file=sys.stderr)
+""")
     while True:
         line = input()
         if not line:
@@ -233,7 +316,7 @@ password
 .
 '''
 to use the default method 'ripemd160'
-""", file=sys.stderr)
+""")
     oper_pass = input()
     oper_method = input()
     if oper_method != "-" and oper_method not in ("ripemd160", "sha1", "md5"):
@@ -245,20 +328,20 @@ If you use z then the user will need to be connected via SSL to oper
 If you use r then the user needs to be identified before opering.
 etc.
 Enter "." if you don't need this directive.
-""", file=sys.stderr)
+""")
     oper_rmodes = input()
     print(
 """
 What connection class should this oper fall under?
 A good default is clients, or an 'oper' class.
-""", file=sys.stderr)
+""")
     oper_class = input()
     print(
 """
 What flags should this oper get?
 At the present time only short flags are implemented.
 THERE IS NO VALIDATION FOR THIS YET
-""", file=sys.stderr)
+""")
     oper_flags = input()
     print(
 """
@@ -267,17 +350,17 @@ If you want:
 Oper is a dinosaur
 Then enter 'is a dinosaur'
 If you don't want a swhois in this block, enter '.'
-""", file=sys.stderr)
+""")
     oper_swhois = input()
-    print("""If the oper or you want to give any default snotices, put their letters here.""", file=sys.stderr)
+    print("""If the oper or you want to give any default snotices, put their letters here.""")
     oper_snomask = input()
-    print("""If you want the oper to get any other default modes, put them here.""", file=sys.stderr)
+    print("""If you want the oper to get any other default modes, put them here.""")
     oper_modes = input()
-    print("""How many clients can use this oper block at the same time?""", file=sys.stderr)
+    print("""How many clients can use this oper block at the same time?""")
     oper_max = input()
     checkInt(oper_max)
-    print("""Printing/Writing""", file=sys.stderr)
-    print(colored("""-----------------------------------""", "red", attrs=["bold"]), file=sys.stderr)
+    print("""Printing/Writing""")
+    print(colored("""-----------------------------------""", "red", attrs=["bold"]))
     print("""oper %s {"""% oper_name)
     print("""    from {""")
     for host in hosts:
@@ -329,19 +412,19 @@ def link():
     print(
 """
 What is the irc.server.name of the server we're connecting to?
-""", file=sys.stderr)
+""")
     link_name = input()
     print("""Username of the account on the remote server.
-If you aren't using ident for identification you can enter '*' here.""", file=sys.stderr)
+If you aren't using ident for identification you can enter '*' here.""")
     link_username = input()
-    print("""Hostname/IP of the remote server""", file=sys.stderr)
+    print("""Hostname/IP of the remote server""")
     link_hostname = input()
     print("""What IP on the local box should we bind to before connecting.
 If you are on shared hosting or whathaveyou and your hosting provider
 told you to use a specific ip when using network services, then use it.
-Otherwise it should be safe to enter '*' here.""", file=sys.stderr)
+Otherwise it should be safe to enter '*' here.""")
     link_bindip = input()
-    print("""What port on the remote machine should we connect to?""", file=sys.stderr)
+    print("""What port on the remote machine should we connect to?""")
     link_port = input()
     checkInt(link_port)
     print("""What password should we use to connect to the remote server?
@@ -349,23 +432,23 @@ And what password should we get back?
 And what method should we use for the recvpass?
 Enter "." for default encryption
 Enter "-" for no encryption
-These will be switched on the other side of the link""", file=sys.stderr)
+These will be switched on the other side of the link""")
     link_connpass = input()
     link_recvpass = input()
     link_method = input()
 
     print("""What class will this link block be in?
-    A good default is 'servers'""", file=sys.stderr)
+    A good default is 'servers'""")
     link_class = input()
-    print("""What options will we use?""", file=sys.stderr)
+    print("""What options will we use?""")
     link_options = []
     while True:
         line = input()
         if not line:
             break
         link_options.append(line)
-    print("""Printing/Redirecting to file""", file=sys.stderr)
-    print(colored("""-------------------------------------""", "red", attrs=["bold"]), file=sys.stderr)
+    print("""Printing/Redirecting to file""")
+    print(colored("""-------------------------------------""", "red", attrs=["bold"]))
     print("""link %s {"""% link_name)
     print("""    username %s;"""% link_username)
     print("""    hostname %s;"""% link_hostname)
@@ -387,23 +470,24 @@ These will be switched on the other side of the link""", file=sys.stderr)
     print("""    };""")
     print("""};""")
 def listen():
-    print("""Here's listen blocks""", file=sys.stderr)
-    print("""What is the IP we're going to listen on?""", file=sys.stderr)
+    print("""Here's listen blocks""")
+    print("""What is the IP we're going to listen on?""")
     listen_ip = input()
-    print("""What port are we listening on?""", file=sys.stderr)
+    print("""What port are we listening on?""")
     listen_port = input()
-    print("""What options will these blocks have?""", file=sys.stderr)
+    print("""What options will these blocks have?""")
     listen_options = []
     while True:
         line = input()
         if not line:
             break
         listen_options.append(line)
-    print("""Printing/Redirecting to file""", file=sys.stderr)
-    print("""---------------------------------------""", file=sys.stderr)
+    print("""Printing/Redirecting to file""")
+    print("""---------------------------------------""")
     print("""listen %s:%s {"""% (listen_ip, listen_port))
     print("""    options {""")
     for option in listen_options:
         print("""        %s;"""% option)
     print("""    };""")
     print("""};""")
+=end
