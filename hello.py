aa2689e37842cf8c2a6b56d2b50615b4ff2d7013
Ken Spencer <iota@e-code.in>
2016-06-09 16:28:38 -0400

    Update hello.py
    Update and Fix

--- hello.py
+++ hello.py
@@ -3,6 +3,17 @@
 import argparse
 import sys
 
+langs = {
+    "en": ["English", "Hello World!"],
+    "sr-la": ["Serbian - Latin", "zdravo svet"],
+    "sr-cy": ["Serbian - Cyrillic", "ждраво свет!"],
+    "es": ["Spanish", "¡Hola Mundo!"],
+    "jp-ro": ["Romanized Japanese", "Sekai kon'nichiwa!"],
+    "jp": ["Japanese", "世界こんにちは！"],
+    "fr": ["French", "Bonjour Monde!"],
+    "nl": ["Dutch", "Hallo Wereld!"],
+    "de": ["German", "Hallo Welt!"],
+}
 def arguments():
     global p
     p = argparse.ArgumentParser(description="Small script for saying hello in different languages")
@@ -12,18 +23,16 @@ def arguments():
 def main():
     if not args.LANG:
         p.print_help()
+        print("Languages:")
+        print("    Code:     Language:")
+        for (k,v) in iter(langs.items()):
+            if len(k) == 5:
+                print("    %s     %s" % (k,v[0]))
+            elif len(k) == 2:
+                print("    %s        %s" % (k,v[0]))
     if args.LANG:
-        dict = {
-        "en": "Hello World!",
-        "sr-la": "zdravo svet",
-        "sr-cy": "ждраво свет!",
-        "es": "¡Hola Mundo!",
-        "jp-ro": "Sekai kon'nichiwa!",
-        "jp": "世界こんにちは！",
-    }
-        print(dict.get(args.LANG))
+        print(langs.get(args.LANG)[1])
 
 if __name__ == "__main__":
     arguments()
     main()
-
